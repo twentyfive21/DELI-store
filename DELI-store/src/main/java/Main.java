628e230e4788173f80fb32d2dcac48e86ef34a9a
twentyfive21 <tina.winnn@gmail.com>
2024-05-21 13:55:22 -0400

    getting classes ready to start capstone

---
+++ DELI-store/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.pluralsight.OrderDetails;
+import com.pluralsight.Sandwich;
+
+import static com.pluralsight.DisplayMenu.displayMenuOptions;
+
+public class Main {
+    // start of the program
+    public static void main(String[] args) {
+        // method calling from DisplayMenu class
+        displayMenuOptions();
+    }
+
+}
---
+++ DELI-store/src/main/java/com/pluralsight/DisplayMenu.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class DisplayMenu {
+    public static void main(String[] args) {
+        displayMenuOptions();
+    }
+
+    public static void displayMenuOptions(){
+        System.out.println("hello from display menu");
+    }
+}
