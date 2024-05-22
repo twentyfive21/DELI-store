c8ec92d3a61c314d58ff2a38b866ea96f8c4a494
twentyfive21 <tina.winnn@gmail.com>
2024-05-22 11:22:00 -0400

    homescreen method created

--- DELI-store/src/main/java/com/pluralsight/showMenu/DisplayMenu.java
+++ DELI-store/src/main/java/com/pluralsight/showMenu/DisplayMenu.java
@@ -6,35 +6,56 @@ import com.pluralsight.menuItems.Sandwich;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class DisplayMenu {
     // keep count of sauce if user adds to order
-    public int aSauce;
+    public static int aSauce;
+    // global scanner
+    static Scanner scan = new Scanner(System.in);
 
     // **************** START OF PROGRAM ****************
 
     public static void main(String[] args) {
         displayMenuOptions();
     }
+
+
     // **************** DISPLAY THE MAIN MENU ****************
     public static void displayMenuOptions(){
-        Sandwich sandwich = new Sandwich("12'Inch", 8.99, "White Bread");
-        List<String> cheeses = List.of("White", "American");
-        sandwich.setCheeses(cheeses);
-        List<String> meats = List.of("Steak", "Chicken");
-        sandwich.setMeats(meats);
-        List<String> toppings = List.of("lettuce", "onions");
-        sandwich.setFreeToppings(toppings);
-        List<String> sauces = List.of("Ranch", "Mayo");
-        sandwich.setFreeSauces(sauces);
-        System.out.println(sandwich);
+//        Sandwich sandwich = new Sandwich("12'Inch", 8.99, "White Bread");
+//        List<String> cheeses = List.of("White", "American");
+//        sandwich.setCheeses(cheeses);
 
-        Chips chips = new Chips("Snack Size", 1.25, "Takis");
+        while (true) {
+            System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("|                 🥪 Welcome to Subway! 🥪              |");
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("|  (1) 🥗 Place a New Order                              |");
+            System.out.println("|  (2) ❌ Exit                                           |");
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.print("|| Please make a selection: ");
+            // users choice
+            String choice = scan.nextLine().trim();
+            switch (choice){
+                case "1": System.out.println("place a new order");
+                    break;
+                case "2": exitProgram();
+                    return;
+                default: System.out.println("\n!!!! Error pick 1 or 2 !!!!");
+                    break;
+            }
+        }
 
-        System.out.println(chips);
+    }
 
-        Drinks drinks = new Drinks("XXL", 6.78, "Big Boke!");
+    public static void exitProgram(){
+        String border = "=========================================================";
+        String message = "||   🍅Thank you for visiting! Have a nice day! :) 🧀  ||";
 
-        System.out.println(drinks);
+        System.out.println();
+        System.out.println(border);
+        System.out.println(message);
+        System.out.println(border);
     }
 }
