4f376594cad37d63a0a5900c723cfca1419fad5f
twentyfive21 <tina.winnn@gmail.com>
2024-05-22 22:57:36 -0400

    util methods added

--- DELI-store/src/main/java/com/pluralsight/utilMethods/UtilMethods.java
+++ DELI-store/src/main/java/com/pluralsight/utilMethods/UtilMethods.java
@@ -1,2 +1,52 @@
-package com.pluralsight.utilMethods;public class UtilMethods {
+package com.pluralsight.utilMethods;
+
+public class UtilMethods {
+
+    // **************************** PRINT OUT MENU ARRAY LIST ****************************
+    public static void printOutMenu(String promptMessage, String[] options){
+        System.out.println("Choose your "+ promptMessage);
+        // display the items available
+        for (int i = 0; i < options.length; i++) {
+            System.out.printf("|  (%d) %s\n", i, options[i]);
+        }
+        // let user pick their selection
+        System.out.print("|| Selection: ");
+    }
+
+
+
+    // **************************** PASS ARRAY BACK ****************************
+
+    public static String[] passArray(String type){
+        String[] selectedArray = null;
+
+        if(type.equals("bread type")){
+            selectedArray = new String[]{"White", "Wheat", "Rye", "Wrap"};
+        }
+
+        if (type.equals("bread size")){
+            selectedArray = new String[] {"4", "8", "12"};
+        }
+
+        if (type.equals("cheese")) {
+            selectedArray = new String[] {"American", "Provolone", "Cheddar", "Swiss"};
+        }
+
+        if (type.equals("meat")){
+            selectedArray = new String[] {"Steak", "Ham", "Salami", "Roast Beef", "Chicken", "Bacon"};
+        }
+
+        if (type.equals("free toppings")) {
+            selectedArray = new String[]{"Lettuce", "Peppers", "Onions", "Tomatoes",
+                    "Jalapenos", "Cucumbers", "Pickles", "Guacamole", "Mushrooms"};
+        }
+
+        if(type.equals("sauces")) {
+            selectedArray = new String[]{"Mayo", "Mustard", "Ketchup", "Ranch", "Thousand Islands", "Vinaigrette"};
+        }
+
+        return selectedArray;
+
+    }
+
 }
