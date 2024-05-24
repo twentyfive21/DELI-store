5ec78a0bd91c26bcc4e8e7008fd57eb1d40cae25
twentyfive21 <tina.winnn@gmail.com>
2024-05-24 13:59:31 -0400

    adding load sig sandwiches

--- DELI-store/src/main/java/Main.java
+++ DELI-store/src/main/java/Main.java
@@ -3,11 +3,13 @@
 
 
 import static com.pluralsight.showMenu.DisplayMenu.display;
+import static com.pluralsight.showMenu.DisplayMenu.loadSignatureSandwiches;
 
 public class Main {
     // ************ start of the program ***************
     public static void main(String[] args) {
         // method calling from DisplayMenu class
+        loadSignatureSandwiches();
         display();
     }
 
