5a222427f730885ac8752d0ca4e7175598fd96ca
twentyfive21 <tina.winnn@gmail.com>
2024-05-24 14:00:21 -0400

    reformatted receipt file for csv

--- DELI-store/src/main/java/com/pluralsight/fileManager/Receipt.java
+++ DELI-store/src/main/java/com/pluralsight/fileManager/Receipt.java
@@ -22,20 +22,35 @@ public class Receipt {
            // Format the date and time
            String formattedDate = now.format(formatter);
            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(formattedDate));
-
-           for (Sandwich sandwich : allSandwiches){
-               bufferedWriter.write(String.valueOf(sandwich));
-               bufferedWriter.newLine();
+           bufferedWriter.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+           bufferedWriter.write("|                        Receipt                         |\n");
+           bufferedWriter.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+           if(allSandwiches.isEmpty()){
+               bufferedWriter.write("| No sandwiches ordered \n");
+           } else {
+               for (Sandwich sandwich : allSandwiches){
+                   bufferedWriter.write(String.valueOf(sandwich) + "\n");
+               }
            }
-
-           bufferedWriter.write(allSandwiches.toString());
-           bufferedWriter.newLine();
-           bufferedWriter.write(allChips.toString());
-           bufferedWriter.newLine();
-           bufferedWriter.write(allDrinks.toString());
-           bufferedWriter.newLine();
+           bufferedWriter.write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+          if (allChips.isEmpty()){
+              bufferedWriter.write("| No chips ordered\n");
+          } else {
+              for (Chips chip: allChips){
+                  bufferedWriter.write(String.valueOf(chip));
+              }
+          }
+           bufferedWriter.write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+          if (allDrinks.isEmpty()){
+              bufferedWriter.write("| No drinks ordered \n");
+          }else {
+              for (Drinks drink : allDrinks){
+                  bufferedWriter.write(String.valueOf(drink));
+              }
+          }
+           bufferedWriter.write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
            bufferedWriter.write("Total Today: $" + String.format("%,.2f", total));
-           bufferedWriter.newLine();
+           bufferedWriter.write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
            bufferedWriter.flush();
            bufferedWriter.close();
        }catch (Exception e){
